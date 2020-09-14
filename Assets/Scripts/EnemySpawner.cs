ed2fa10cda3bff850bbcdc527127500ada558e18
Virunee <viravongv@gmail.com>
2020-09-15 00:51:12 +0100

    set up initial enemy spawner

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] List<WaveConfig> waveConfigs;
+
+    int startingWave = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var currentWave = waveConfigs[startingWave];
+        StartCoroutine(SpawnAllEnemiesInWave(currentWave));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator SpawnAllEnemiesInWave(WaveConfig waveConfig)
+    {
+        for(var enemyCount=0; enemyCount < waveConfig.getnumberOfEnemies(); enemyCount++)
+        {
+            Instantiate(
+            waveConfig.getEnemyPrefab(),
+            waveConfig.getWaypoints()[0].transform.position,
+            Quaternion.identity);
+
+            yield return new WaitForSeconds(waveConfig.getTimeBetweenSpawns());
+        }
+        
+
+    }
+}
