6d52e456951868394ffc1ba8ed2c5959a5fa6f95
Virunee <viravongv@gmail.com>
2020-09-17 16:59:59 +0100

    make enemies fire

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,27 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.UIElements;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField] float health = 100f;
+    [SerializeField] float shotCounter;
+    [SerializeField] float minTimeBetweenShots = 0.2f;
+    [SerializeField] float maxTimeBetweenShots = 3f;
+    [SerializeField] GameObject laserPrefab;
+    [SerializeField] float projectileSpeed = 1f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
     }
 
     // Update is called once per frame
     void Update()
     {
+        countdownAndShoot();
+    }
 
+    private void countdownAndShoot()
+    {
+        shotCounter -= Time.deltaTime;
+        if(shotCounter <= 0)
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        GameObject laser = Instantiate(laserPrefab, transform.position, Quaternion.identity) as GameObject;
+        laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -projectileSpeed);
+        shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
     }
 
     private void OnTriggerEnter2D(Collider2D otherObject)
     {
         DamageDealer damageDealer = otherObject.gameObject.GetComponent<DamageDealer>();
-        Destroy(otherObject.gameObject);
         ProcessHit(damageDealer);
+
+        if(!otherObject.name.Contains("Enemy"))
+        {
+            Destroy(otherObject.gameObject);
+        }
     }
 
     private void ProcessHit(DamageDealer damageDealer)
